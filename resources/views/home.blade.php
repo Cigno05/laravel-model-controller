31877065826dbc5e3fa6d24fca3fcbaafc11907e
Cigno05 <paolocalcagni.82@gmail.com>
2024-05-23 19:18:44 +0200

    ADD card to home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,22 @@ Home
 
 @section('content')
 
-<h1>Siamo nella Homepage</h1>
+<div class="container">
+    <div class="row">
+        @foreach ($movies as $movie)
+        <div class="col-xl-3 col-lg-4 col-md-6 col-12 card-group">
+            <div class="card mb-5">
+                <div class="card-body">
+                    <h3>{{ $movie->title }}</h3>
+                    <h5>{{ $movie->original_title }}</h5>
+                    <h5>{{ $movie->vote }}</h5>
+                    <h5>Uscito il: {{ $movie->date }}</h5>
+                </div>
+            </div>
+        </div>
+        
+        @endforeach
+    </div>
+</div>
 
 @endsection
